1f51f08e144cccabf516bb447c298cb05c64f6bf
Gilbert Chan <thegilbertchan@gmail.com>
2017-04-15 02:20:44 -0700

    Styled all pages and FA Install

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   # get '/' => 'pages#home'
   get 'about' => 'pages#about'
+  get 'homealt' => 'pages#homealt'
   get 'contact' => 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
