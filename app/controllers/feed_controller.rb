45015e85da8665e64c1b55f0815f8d9205137f3e
Gilbert Chan <thegilbertchan@gmail.com>
2017-05-04 19:14:48 -0700

    fixed follow

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,6 +1,9 @@
 class FeedController < ApplicationController
    before_action :authenticate_user!
   def show
-  		@forum_threads = ForumThread.where(user: current_user.all_following)
+  		@forum_threads = current_user.following_forum_threads
+  		# @forum_threads = ForumThread.where(id:current_user.follows_by_type('ForumThread').map(&:followable_id))
+  		# @forum_threads = current_user.follows_by_type('ForumThread').map{|d| ForumThread.find_by(id: d.followable_id)}
+
   end
 end
